cf7bc6b24d0ae5c8d7bba20a47b24e0a5b876bc8
KMmiau <143568357+KMmiau@users.noreply.github.com>
2023-11-24 18:47:00 -0600

    Add files via upload

---
+++ EVA3_4_FUNCION_ARREGLO/src/eva3_4_funcion_arreglo/EVA3_4_FUNCION_ARREGLO.java
@@ -0,0 +1,21 @@
+
+package eva3_4_funcion_arreglo;
+
+public class EVA3_4_FUNCION_ARREGLO {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = crearArreglo(10, 100);
+        
+        for (int valor : arreglo) {
+            System.out.print("[" + valor + "]");
+        }
+    }
+public static int[] crearArreglo(int tamaño, int rango){
+    int[] arreglo = new int[tamaño];
+    for (int i = 0; i < arreglo.length; i++) {
+        arreglo[i] = (int)(Math.random() * rango);
+    }
+    return arreglo;
+}
+}
